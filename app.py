e59f38bb46282347162534880373be19731c0333
Izhar Firdaus <kagesenshi.87@gmail.com>
2020-11-06 16:16:09 +0800

    initial import

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+from flask import Response
+import requests
+import os
+import mimetypes
+
+pypi_upstream = 'https://pypi.python.org/simple/'
+files_upstream = 'https://files.pythonhosted.org/'
+cache_dir = os.path.join(os.path.dirname(__file__), 'cache')
+
+app = Flask('PyPI Caching Proxy')
+
+def rewrite_response(text):
+    root = request.url_root
+    return text.replace(files_upstream, root + 'files/')
+
+@app.route('/simple', defaults={'path': ''}, methods=['GET'])
+@app.route('/simple/<path:path>', methods=['GET'])
+def get_pypi(path):
+    upstream = pypi_upstream + path + '?' + request.query_string.decode('utf8')
+    print('Fetching upstream %s' % upstream)
+    r = requests.get(upstream)
+    return Response(rewrite_response(r.text), mimetype=r.headers.get('Content-Type'))
+
+
+@app.route('/files', defaults={'path': ''}, methods=['GET'])
+@app.route('/files/<path:path>', methods=['GET'])
+def get_files(path):
+    cache_file = os.path.join(cache_dir, path)
+    item_dir = os.path.dirname(cache_file)
+    if not os.path.exists(item_dir):
+        os.makedirs(item_dir)
+    if os.path.exists(cache_file):
+        mimetype = mimetypes.guess_type(path)[0] or 'application/octet-stream'
+        print('Fetching from cache %s' % cache_file)
+        with open(cache_file, 'rb') as f:
+            return Response(f.read(), mimetype=mimetype)
+    upstream = files_upstream + path + '?' + request.query_string.decode('utf8')
+    print('Fetching upstream %s' % upstream)
+    r = requests.get(upstream)
+    with open(cache_file, 'wb') as f:
+        f.write(r.content)
+    return Response(r.content, mimetype=r.headers.get('Content-Type'))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
